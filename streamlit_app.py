7f40c14c18c287e2b9f0f126e09447473452a171
Дмитрий Бороздин <46523802+dborozdin@users.noreply.github.com>
2024-04-18 17:34:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta
+import string
+import time
 
+data = [{'Geeks': 'dataframe', 'For': 'using', 'geeks': 'list'}, 
+        {'Geeks':10, 'For': 20, 'geeks': 30}]  
+  
+df = pd.DataFrame.from_dict(data) 
+df.head()
