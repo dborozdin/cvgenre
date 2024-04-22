56d26b93d8656b712b72d85f5a99a7b6e8538e68
Дмитрий Бороздин <46523802+dborozdin@users.noreply.github.com>
2024-04-22 10:39:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,8 +101,9 @@ if uploaded_files:
         
         test_file_path= uploaded_file.name
         st.write("Filename: ", test_file_path)
-        
-        image = Image.open(test_file_path)
+       
+        image_data = uploaded_file.getvalue()
+        image = Image.frombytes('RGBA', (300,300), image_data, 'raw') #Image.open(test_file_path)
         #Extract the features
         with torch.no_grad():
             inputs = processor(images=image, return_tensors="pt").to(device)
